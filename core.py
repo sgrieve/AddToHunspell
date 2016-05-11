198578746b3efc04e1e60c9774901b3c904be7f9
sgrieve <s.grieve@ed.ac.uk>
2016-05-11 21:22:34 +0100

    docstrings and error handling

--- core.py
+++ core.py
@@ -4,6 +4,10 @@ import sys
 
 def IdentifySeparator(RawWords):
     """
+    Pass in a string of RawWords to identify the separator. Will only find
+    commas (with or without a trailing space), spaces, and newlines.
+
+    Returns the separator as a string.
     """
     if (',' in RawWords or ', ' in RawWords):
         return ','
@@ -12,10 +16,8 @@ def IdentifySeparator(RawWords):
     elif '\n' in RawWords:
         return '\n'
     else:
-        print
-        print 'Word list is not correctly formatted, separator not found.'
-        print 'Use commas, spaces or new lines.'
-        print
+        print '\nWord list is not correctly formatted, separator not found.'
+        print 'Use commas, spaces or new lines.\n'
         sys.exit()
 
 
@@ -34,18 +36,26 @@ def MakeBackup():
 
 def LoadWordList(filename):
     """
+    Take a filename and load the words contained in that file, returning a list
+    of words, cleaned to remove separators and whitespace.
     """
-    with open(filename, 'r') as f:
-        RawWords = f.read()
+    try:
+        with open(filename, 'r') as f:
+            RawWords = f.read()
 
-    RawWords = RawWords.strip()
-    separator = IdentifySeparator(RawWords)
+        RawWords = RawWords.strip()
+        separator = IdentifySeparator(RawWords)
 
-    return CleanWords(RawWords.split(separator))
+        return CleanWords(RawWords.split(separator))
+    except:
+        print '\nWord list file - {} - not found.\n'.format(filename)
+        sys.exit()
 
 
 def CleanWords(WordList):
     """
+    Pass in a wordlist and this method will return the same list of words,
+    with no whitespace either side of each word.
     """
     return [word.strip() for word in WordList]
 
@@ -59,3 +69,4 @@ def AppendWordsToDict():
 print LoadWordList('Geomorphology.txt')
 print LoadWordList('Geomorphology2.txt')
 print LoadWordList('Geomorphology3.txt')
+print LoadWordList('Geomorphology4.txt')
